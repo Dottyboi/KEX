47206cbfa23422f7565c8b1433d7991c3e4683e5
Dottyboi <Fabian.pizarro.04@gmail.com>
2026-03-06 15:05:31 +0100

    testing video files

--- test.py
+++ test.py
@@ -1,24 +1,13 @@
-# STEP 1: Import the necessary modules.
-import mediapipe as mp
-from mediapipe.tasks import python
-from mediapipe.tasks.python import vision
-import cv2
-from google.colab.patches import cv2_imshow
+from pose_format import Pose
+from pose_format.pose_visualizer import PoseVisualizer
 
-img = cv2.imread("image.jpg")
-cv2_imshow(img)
-# STEP 2: Create an HandLandmarker object.
-base_options = python.BaseOptions(model_asset_path='hand_landmarker.task')
-options = vision.HandLandmarkerOptions(base_options=base_options,
-                                       num_hands=2)
-detector = vision.HandLandmarker.create_from_options(options)
 
-# STEP 3: Load the input image.
-image = mp.Image.create_from_file("image.jpg")
+with open("example.pose", "rb") as f:
+    pose = Pose.read(f.read())
 
-# STEP 4: Detect hand landmarks from the input image.
-detection_result = detector.detect(image)
+v = PoseVisualizer(pose)
 
-# STEP 5: Process the classification result. In this case, visualize it.
-annotated_image = draw_landmarks_on_image(image.numpy_view(), detection_result)
-cv2_imshow(cv2.cvtColor(annotated_image, cv2.COLOR_RGB2BGR))
+v.save_video("example.mp4", v.draw())
+
+# Draws pose on top of video. 
+v.save_video("example.mp4", v.draw_on_video("SSLC\SSLC_videofiler_mp4\SSLC01_004_S001_S002_p.mp4"))
