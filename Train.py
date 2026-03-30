e83810dcb5d99bfe910969f4fb7208d68dc6d027
Dottyboi <Fabian.pizarro.04@gmail.com>
2026-03-30 11:44:36 +0200

    Update Train.py

--- Train.py
+++ Train.py
@@ -2,7 +2,7 @@ import torch
 from pose_format.pose import Pose
 import pose_format
 print(dir(pose_format))
-from pose_format.torch import TorchPoseBody
+from pose_format.torch import pose_body as TorchPoseBody
 
 data_buffer = open("example.pose", "rb").read()
 
