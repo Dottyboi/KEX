c58bcbb504796a410e76352b29968333dd2da299
Dottyboi <Fabian.pizarro.04@gmail.com>
2026-03-30 13:49:41 +0200

    Update Train.py

--- Train.py
+++ Train.py
@@ -5,5 +5,5 @@ from pose_format.torch.pose_body import TorchPoseBody
 data_buffer = open("example.pose", "rb").read()
 
 # Load data as a PyTorch tensor:
-pose = Pose.read(data_buffer, TorchPoseBody)
+pose = Pose.read(data_buffer, TorchPoseBody).torch()
 print(type(pose))
